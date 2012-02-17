XQuartz/quartz-wm

commit 682660f84093f06f32515b1d01b649daa2e58fb1

Use "intended" frame rect to determine if window is maximized.
The intended frame is that which is queued, pending, or current, in that
order.

This fixes a problem where _NET_WM_STATE_MAXIMIZED_{HORZ,VERT} were
incorrect. Changes to the frame can be delayed.  A sequence like the
following occurred: initiate frame rect change for zoom or maximize, set
_NET_WM_STATE based in part on current frame, frame rect change actually
takes effect.  It leaves _NET_WM_STATE_MAXIMIZED_{HORZ,VERT} in the
exact opposite state as it should be.  Later, a property notify causes a
call to -update_frame, which calls -update_net_wm_state_hints, which
uses the incorrect _NET_WM_STATE to change the window's actual state.
So, the window might spontaneously maximize when it shouldn't.

Fixes <rdar://problem/10148457>

Reviewed-by: Jeremy Huddleston <jeremyhu@apple.com>

src/x-window.m
@@ -57,6 +57,7 @@
 - (void) decorate_rect:(X11Rect)or;
 - (xp_frame_class) get_xp_frame_class;
 - (void) map_unmap_client;
+- (BOOL) is_maximized;
 - (void) do_maximize;
 - (void) do_fullscreen:(BOOL)flag;
 - (void) do_uncollapse_and_tell_dock:(BOOL)tell_dock with_animation:(BOOL)anim;
@@ -67,6 +68,7 @@
                       from_user:(BOOL)uflag constrain:(BOOL)cflag;
 - (X11Rect) frame_inner_rect:(X11Rect)or;
 - (X11Rect) client_rect:(X11Rect)or;
+- (X11Rect) intended_frame;
 @end
 
 static const char *gravity_type(int gravity) {
@@ -1418,7 +1420,7 @@ ENABLE_EVENTS (_id, X_CLIENT_WINDOW_EVENTS)
     }
     if(_fullscreen)
         _atoms[n_atoms++] = atoms.net_wm_state_fullscreen;
-    if(X11RectEqualToRect(_current_frame, [_screen zoomed_rect:X11RectOrigin(_current_frame)])) {
+    if([self is_maximized]) {
         _atoms[n_atoms++] = atoms.net_wm_state_maximized_horz;
         _atoms[n_atoms++] = atoms.net_wm_state_maximized_vert;
     }
@@ -1459,7 +1461,7 @@ ENABLE_EVENTS (_id, X_CLIENT_WINDOW_EVENTS)
             x_remove_window_from_menu (self);
     } else if(state == atoms.net_wm_state_maximized_horz ||
               state == atoms.net_wm_state_maximized_vert) {
-        BOOL maximized = X11RectEqualToRect(_current_frame, [_screen zoomed_rect:X11RectOrigin(_current_frame)]);
+        BOOL maximized = [self is_maximized];
         if(mode == 1 || (mode == 2 && !maximized))
             [self do_maximize];
         else if(maximized)
@@ -2291,21 +2293,28 @@ ENABLE_EVENTS (_id, X_CLIENT_WINDOW_EVENTS)
         [self do_unshade:timestamp];
 }
 
+- (BOOL) is_maximized
+{
+    X11Rect r = [self intended_frame];
+    return X11RectEqualToRect(r, [_screen zoomed_rect:X11RectOrigin(r)]);
+}
+
 - (void) do_zoom {
-    X11Rect maximized_rect = [self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(_current_frame)]];
-    X11Rect new_rect = maximized_rect;
+    X11Rect r = [self intended_frame];
+    X11Rect new_rect;
 
     TRACE ();
 
     if (!_has_unzoomed_frame) {
-        _unzoomed_frame = _current_frame;
+        _unzoomed_frame = r;
         _has_unzoomed_frame = YES;
     }
 
-    if(X11RectEqualToRect(_current_frame, maximized_rect)) {
+    if([self is_maximized]) {
         new_rect = [self validate_frame_rect:_unzoomed_frame];
     } else {
-        _unzoomed_frame = _current_frame;
+        new_rect = [self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(r)]];
+        _unzoomed_frame = r;
     }
 
     [self resize_frame:new_rect];
@@ -2314,12 +2323,14 @@ ENABLE_EVENTS (_id, X_CLIENT_WINDOW_EVENTS)
 }
 
 - (void) do_maximize {
-    X11Rect maximized_rect = [self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(_current_frame)]];
 
     TRACE ();
 
-    if(!X11RectEqualToRect(_current_frame, maximized_rect)) {
-        _unzoomed_frame = _current_frame;
+    if(![self is_maximized]) {
+        X11Rect r = [self intended_frame];
+        X11Rect maximized_rect = [self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(r)]];
+
+        _unzoomed_frame = r;
         _has_unzoomed_frame = YES;
 
         [self resize_frame:maximized_rect];
@@ -2334,7 +2345,6 @@ ENABLE_EVENTS (_id, X_CLIENT_WINDOW_EVENTS)
  * the actual frame for Xplugin (done in update_frame).
  */
 - (void) do_fullscreen:(BOOL) flag {
-    X11Rect maximized_rect = [self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(_current_frame)]];
 
     DB("id: 0x%lx frame_id: 0x%lx currently: %d requested: %d", _id, _frame_id, _fullscreen, flag);
 
@@ -2350,7 +2360,10 @@ ENABLE_EVENTS (_id, X_CLIENT_WINDOW_EVENTS)
         return;
 
     if(flag) {
-        _unzoomed_frame = _current_frame;
+        X11Rect r = [self intended_frame];
+        X11Rect maximized_rect = [self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(r)]];
+
+        _unzoomed_frame = r;
         _has_unzoomed_frame = YES;
 
         [self resize_frame:maximized_rect force:YES];
@@ -2634,6 +2647,20 @@ decode_size_hints (XSizeHints *hints, int base[2], int min[2],
         return [NSString stringWithFormat:@"{x-window %@}", _title];
     else
         return [NSString stringWithFormat:@"{x-window 0x%x}", _id];
+}
+
+- (X11Rect) intended_frame
+{
+    X11Rect r;
+
+    if (_queued_frame_change)
+        r = _queued_frame;
+    else if (_pending_frame_change)
+        r = _pending_frame;
+    else
+        r = _current_frame;
+
+    return r;
 }
 
 @end

commit 24818cfbc7e345bd357e553a1082fd4530284412

Fix "un-zooming" on certain windows which weren't reacting properly
Regression from: 682660f84093f06f32515b1d01b649daa2e58fb1

The prior code sometimes tested for a maximized window by comparing the
current frame with

	[_screen zoomed_rect:X11RectOrigin(_current_frame)]

but other times compared it with

	[self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(_current_frame)]]

The latter is more correct and takes WM size hints, like the resize
increments, into account.  My code standardized on the former, though.  The
old code was wrong in some cases, mine is wrong in all cases.

The long story:

My understanding is that quartz-wm's behavior is based on Cocoa's.
NSWindow's -isZoomed method tests if the window's frame equals the zoomed
frame computed for the window.  So, any change of the window's frame
potentially changes whether it is considered zoomed or not.  Quartz-wm
behaves similarly.

There are two aspects of what "is considered zoomed" might mean.  There's the
method in the above patch, -is_maximized, in the quartz-wm code.  That
compares the current frame (well, "intended" frame) with a computed zoomed
frame.  Then, there's the question of whether quartz-wm puts the
_NET_WM_STATE_MAXIMIZED_{HORZ,VERT} atoms in the window's _NET_WM_STATE
property.

My changes were intended to get the latter part to match the former.

However, I also inadvertently made the computed zoomed frame not meet the
is-it-zoomed test for windows with resize increments and, probably, a maximum
size smaller than the screen.  Quartz-wm would compute one zoomed frame for
the window, set the frame to that, and then subsequently compare against a
possibly different frame to determine if the window was zoomed.

Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>
Reviewed-by: Jeremy Huddleston <jeremyhu@apple.com>

src/x-window.m
@@ -2300,7 +2300,7 @@ ENABLE_EVENTS (_id, X_CLIENT_WINDOW_EVENTS)
 - (BOOL) is_maximized
 {
     X11Rect r = [self intended_frame];
-    return X11RectEqualToRect(r, [_screen zoomed_rect:X11RectOrigin(r)]);
+    return X11RectEqualToRect(r, [self validate_frame_rect:[_screen zoomed_rect:X11RectOrigin(r)]]);
 }
 
 - (void) do_zoom {